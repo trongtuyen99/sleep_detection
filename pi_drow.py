710102e4f82edbd91fc376ebb130a459082d90b1
trongtuyen99 <35034919+trongtuyen99@users.noreply.github.com>
2020-03-03 08:54:45 +0700

    Update pi_drow.py

--- pi_drow.py
+++ pi_drow.py
@@ -11,12 +11,13 @@ import os
 import cv2
 
 # Cau hinh duong dan den file alarm.wav
-wav_path = "/home/pi/miai/landmark/alarm.wav"
+wav_path = "alarm.wav" # path on colab
 
 # Ham phat ra am thanh
 def play_sound(path):
-	os.system('aplay ' + path)
-
+	playsound.playsound(path)
+	# os.system('aplay ' + path)
+ 
 # Ham tinh khoang cach giua 2 diem
 def e_dist(pA, pB):
 	return np.linalg.norm(pA - pB)
@@ -34,55 +35,36 @@ def eye_ratio(eye):
 
 	return eye_ratio_val
 
-# Dinh nghia muc threshold ty le giua doc/ngang. Neu duoi muc nay la ngu gat
-eye_ratio_threshold = 0.25
-
-# Threshold so frame lien tuc nham mat
+eye_ratio_threshold = 0.25 # fix for another man
 max_sleep_frames = 16
-
-# Dem so frame ngu
 sleep_frames = 0
-
-# Check xem da canh bao hay chua
 alarmed = False
 
-# Khoi tao cac module detect mat va facial landmark
 face_detect = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 landmark_detect = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
 
-# Lay danh sach cac cum diem landmark cho 2 mat
 (left_eye_start, left_eye_end) = face_utils.FACIAL_LANDMARKS_IDXS["left_eye"]
 (right_eye_start, right_eye_end) = face_utils.FACIAL_LANDMARKS_IDXS["right_eye"]
 
 # Doc tu camera
-vs = VideoStream(src=0).start()
-time.sleep(1.0)
-
+video_capture = cv2.VideoCapture(0)
+print(video_capture)
 while True:
-
-	# Doc tu camera
-	frame = vs.read()
-
-	# Resize de tang toc do xu ly
+	ret, frame = video_capture.read() 
+	if frame is None:
+		break
+		
 	frame = imutils.resize(frame, width=450)
-
-	# Chuyen ve gray
 	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+	faces = face_detect.detectMultiScale(gray, scaleFactor=1.1,minNeighbors=5, minSize=(100, 100),flags=cv2.CASCADE_SCALE_IMAGE);
 
-	# Detect cac mat trong anh
-	faces = face_detect.detectMultiScale(gray, scaleFactor=1.1,		minNeighbors=5, minSize=(100, 100),		flags=cv2.CASCADE_SCALE_IMAGE)
-
-	# Duyet qua cac mat
 	for (x, y, w, h) in faces:
-
-		# Tao mot hinh chu nhat quanh khuon mat
-		rect = dlib.rectangle(int(x), int(y), int(x + w),
-			int(y + h))
-
+    # show recangle around face
+		cv2.rectangle(frame, (x,y), (x+w, y+h), (0,255,0),2)
+		rect = dlib.rectangle(int(x), int(y), int(x + w),int(y + h))
 		# Nhan dien cac diem landmark
 		landmark = landmark_detect(gray, rect)
 		landmark = face_utils.shape_to_np(landmark)
-
 		# Tinh toan ty le mat phai va trai va trung binh cong 2 ratio
 		leftEye = landmark[left_eye_start:left_eye_end]
 		rightEye = landmark[right_eye_start:right_eye_end]
@@ -97,18 +79,13 @@ while True:
 		right_eye_bound = cv2.convexHull(rightEye)
 		cv2.drawContours(frame, [left_eye_bound], -1, (0, 255, 0), 1)
 		cv2.drawContours(frame, [right_eye_bound], -1, (0, 255, 0), 1)
-
 		# Check xem mat co nham khong
 		if eye_avg_ratio < eye_ratio_threshold:
 			sleep_frames += 1
-			# if the eyes were closed for a sufficient number of
-			# frames, then sound the alarm
 			if sleep_frames >= max_sleep_frames:
-
 				if not alarmed:
 					alarmed = True
 					# Duong dan den file wav
-
 
 					# Tien hanh phat am thanh trong 1 luong rieng
 					t = Thread(target=play_sound,
@@ -117,26 +94,24 @@ while True:
 					t.start()
 
 				# Ve dong chu canh bao
-				cv2.putText(frame, "BUON NGU THI DI NGU DI ONG OI!!!", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+
+				cv2.putText(frame, "DI NGU THOI CHU NGOI LAM GI!!!",\
+                (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
 
 		# Neu khong nham mat thi
 		else:
-
 			# Reset lai cac tham so
 			sleep_frames = 0
 			alarmed = False
 
 			# Hien thi gia tri eye ratio trung binh
 			cv2.putText(frame, "EYE AVG RATIO: {:.3f}".format(eye_avg_ratio), (10, 30),	cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 0, 0), 2)
-
-	# Hien thi len man hinh
-	cv2.imshow("Camera", frame)
-
-	# Bam Esc de thoat
+	  
+		cv2_imshow(frame)
+	  # Bam Esc de thoat
 	key = cv2.waitKey(1) & 0xFF
 	if key == 27:
 		break
 
-
+video_capture.release()
 cv2.destroyAllWindows()
-vs.stop()
